cf66f0f2e7e93102cd258a8687a0d6a37fdd9470
Anshuman Kumar <anshinanu@yahoo.com>
2020-12-15 19:24:58 +0530

    created the JS file

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputTxt = document.querySelector("#input-txt");
+var outputTxt = document.querySelector("#output-txt");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getConvertedURL(input) {
+    return serverURL + "?text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("ERROR", error);
+    alert("something went wrong with the server. Try again after sometime");
+}
+
+function clickHandler() {
+    var input = inputTxt.value;
+    // console.log(getConvertedURL(input));
+
+    fetch(getConvertedURL(input))
+        .then( response => response.json())
+        .then( json => {
+            var translation = json.contents.translated;
+            outputTxt.innerText = translation;
+        })
+        .catch(errorHandler); 
+}
+
+btnTranslate.addEventListener("click", clickHandler);
